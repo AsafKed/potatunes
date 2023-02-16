85eb900f548dbfc367a8f4de58dcf69716477838
AsafKed <asafkedem97@gmail.com>
2023-02-16 19:50:37 +0100

    Try spotipy, fail

--- app.py
+++ app.py
@@ -1,9 +1,101 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, Docker!'
+
+# # TODO add a login for spotify, with cookies saving the user's spotify id as the user's id
+# @app.route('/login')
+# def login():
+#     return 'Login'
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import os
+from flask import Flask, session, request, redirect, abort
+from flask_session import Session
+import spotipy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+# Session(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def index():
+    # log the message "Hello World!" to docker logs
+    app.logger.info('Hello World!')
 
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 2. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 1. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 3. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+           f'<a href="/playlists">my playlists</a> | ' \
+           f'<a href="/currently_playing">currently playing</a> | ' \
+        f'<a href="/current_user">me</a>' \
+
+
+@app.route('/sign_out')
+def sign_out():
+    session.pop("token_info", None)
+    return redirect('/')
+
+
+@app.route('/playlists')
+def playlists():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user_playlists()
+
+
+@app.route('/currently_playing')
+def currently_playing():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    track = spotify.current_user_playing_track()
+    if not track is None:
+        return track
+    return "No track currently playing."
+
+
+# Added to allow the redirect to the Spotify login page
+@app.after_request
+def add_security_headers(response):
+    # TODO make a CSP policy that allows the redirect to spotify's authentification page using this as a reference: csp.withgoogle.com/docs/strict-csp.html
+    # response.headers['Content-Security-Policy'] = "default-src 'https' ; script-src 'self' 'http'" # for allowing the redirect to spotify's authentification page
+    # response.headers['Content-Security-Policy'] = "script-src 'unsafe-inline'" # for allowing the redirect to spotify's authentification page
+    # response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+'''
+Following lines allow application to be run more conveniently with
+`python app.py` (Make sure you're using python3)
+(Also includes directive to leverage pythons threading capacity.)
+'''
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, port=int(os.environ.get("PORT", os.environ.get("SPOTIPY_REDIRECT_URI", 5000).split(":")[-1]).split("/")[0]), debug=True)
