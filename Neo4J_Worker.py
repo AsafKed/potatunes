629865ecde9f37174ce967fea290930f572738bf
AsafKed <asafkedem97@gmail.com>
2023-03-28 20:33:10 +0200

    Build session neo4j functions

--- Neo4J_Worker.py
+++ Neo4J_Worker.py
@@ -54,6 +54,102 @@ class App:
         person = result[0]
         return person
 
+    def create_session(self, session_id):
+        with self.driver.session(database="neo4j") as session:
+            result = session.execute_write(
+                self._create_and_return_session, session_id)
+
+            print("Result in main method:", result)
+
+        # If creation date is null, add it
+        if result["created"] is None:
+            self.add_creation_date(session_id)
+            result = self.get_session(session_id)
+            print("Result after adding creation date:", result)
+        return result
+        
+    @staticmethod
+    def _create_and_return_session(tx, session_id):
+        query = (
+            """ MERGE (s:Session { session_id: $session_id }) 
+                RETURN s.session_id AS session_id, s.created AS created
+            """
+        )
+        result = tx.run(query, session_id=session_id).data()
+        Uniqueness_Check(result)
+        return result[0]
+    
+    def get_session(self, session_id):
+        with self.driver.session(database="neo4j") as session:
+            result = session.execute_read(
+                self._get_session, session_id)
+
+            return result
+        
+    @staticmethod
+    def _get_session(tx, session_id):
+        query = (
+            """ MATCH (s:Session { session_id: $session_id })
+                RETURN s.session_id AS session_id, s.created AS created
+            """
+        )
+        result = tx.run(query, session_id=session_id).data()
+        Uniqueness_Check(result)
+        return result[0]
+    
+    def add_creation_date(self, session_id):
+        with self.driver.session(database="neo4j") as session:
+            result = session.execute_write(
+                self._add_creation_date, session_id)
+
+            return result
+        
+    @staticmethod
+    def _add_creation_date(tx, session_id):
+        # Get today's date in the format YYYY-MM-DD
+        today = datetime.today().strftime('%Y-%m-%d')
+
+        query_date = (
+            """ MATCH (s:Session { session_id: $session_id })
+                WHERE s.created IS NULL
+                SET s.created = $today
+                RETURN s.session_id AS session_id, s.created AS created
+            """
+        )
+        result = tx.run(query_date, session_id=session_id, today=today).data()
+        Uniqueness_Check(result)
+        return result[0]
+    
+    # def add_user_to_session(self, user_id, session_id):
+    #     with self.driver.session(database="neo4j") as session:
+    #         result = session.execute_write(
+    #             self._add_user_to_session, user_id, session_id)
+
+    #         return result
+        
+    # @staticmethod
+    # def _add_user_to_session(tx, user_id, session_id):
+    #     # Get today's date in the format YYYY-MM-DD
+    #     today = datetime.today().strftime('%Y-%m-%d')
+
+    #     query = (
+    #         """ MATCH (p:Person { user_id: $user_id })
+    #             MATCH (s:Session { session_id: $session_id, date: $today })
+    #             MERGE (p)-[r:ATTENDED { date: $today }]->(s)
+    #             RETURN p.name AS name, p.user_id AS user_id, s.session_id AS session_id, r.date AS date
+    #         """
+    #     )
+    #     result = tx.run(query, user_id=user_id, session_id=session_id, today=today)
+        
+    #     # Turn the result into a list of dictionaries
+    #     result = result.data()
+        
+    #     # Check that only one person with this name and id exists
+    #     Uniqueness_Check(result)
+        
+    #     person = result[0]
+    #     return person
+
     # def create_user_and_session(self, name, user_id, image_url, session_id):
     #     # Create user and make a relation to the session
     #     with self.driver.session(database="neo4j") as session:
